10df9a202be48f6514a98d50b9f119f06cb6a9f0
mwj <yomingyo@gmail.com>
2017-11-28 19:08:35 +0800

    修改test

--- znjf-external-service/src/test/java/com/znjf33/external/api/provider/service/SupplyChainLmjServiceTest.java
+++ znjf-external-service/src/test/java/com/znjf33/external/api/provider/service/SupplyChainLmjServiceTest.java
@@ -1,6 +1,7 @@
 package com.znjf33.external.api.provider.service;
 
 import com.znjf33.external.api.dto.SupplyChainLmjParamDTO;
+import com.znjf33.external.api.dto.SupplyChainLmjResultDTO;
 import com.znjf33.external.api.provider.config.JunitBase;
 import com.znjf33.external.api.service.SupplyChainLmjService;
 import org.junit.Assert;
@@ -29,8 +30,8 @@ public class SupplyChainLmjServiceTest extends JunitBase {
         supplyChainLmjParamDTO.setQuanwangtongYizhifuTransfer("http://www.lemuji.com/qwtys/api/bestpay/transfer_ZNJF.php");
         supplyChainLmjParamDTO.setLmjApiSecretKey("77ed904243c2fde0fb309252e98a960c");
         supplyChainLmjParamDTO.setQuanwangtongYizhifuPartnerid("e40dfb3c25fa31ce3a226695414cbbc3");
-        boolean b = supplyChainLmjService.getCallbackPayStatus(supplyChainLmjParamDTO);
-        Assert.assertTrue(b);
+        SupplyChainLmjResultDTO b = supplyChainLmjService.getCallbackPayStatus(supplyChainLmjParamDTO);
+        Assert.assertNotNull(b);
     }
 
     /**
@@ -45,8 +46,8 @@ public class SupplyChainLmjServiceTest extends JunitBase {
         supplyChainLmjParamDTO.setQuanwangtongYizhifuChargefb("http://www.lemuji.com/qwtys/api/bestpay/chargeFB_ZNJF.php");
         supplyChainLmjParamDTO.setLmjApiSecretKey("77ed904243c2fde0fb309252e98a960c");
         supplyChainLmjParamDTO.setQuanwangtongYizhifuPartnerid("e40dfb3c25fa31ce3a226695414cbbc3");
-        boolean b = supplyChainLmjService.getCallbackPayStatus(supplyChainLmjParamDTO);
-        Assert.assertTrue(b);
+        SupplyChainLmjResultDTO b = supplyChainLmjService.getCallbackPayStatus(supplyChainLmjParamDTO);
+        Assert.assertNotNull(b);
     }
 
     /**
@@ -61,8 +62,8 @@ public class SupplyChainLmjServiceTest extends JunitBase {
         supplyChainLmjParamDTO.setLmjIdentifyPartnerid("e40dfb3c25fa31ce3a226695414cbbc3");
         supplyChainLmjParamDTO.setLmjApiSecretKey("77ed904243c2fde0fb309252e98a960c");
         supplyChainLmjParamDTO.setLmjUrl("http://www.lemuji.com/test_qwtys/api/znjf/znjf.php");
-        boolean b = supplyChainLmjService.getCallbackPayStatus(supplyChainLmjParamDTO);
-        Assert.assertTrue(b);
+        SupplyChainLmjResultDTO b = supplyChainLmjService.getCallbackPayStatus(supplyChainLmjParamDTO);
+        Assert.assertNotNull(b);
     }
 
     /**
@@ -77,8 +78,8 @@ public class SupplyChainLmjServiceTest extends JunitBase {
         supplyChainLmjParamDTO.setLmjApiSecretKey("77ed904243c2fde0fb309252e98a960c");
         supplyChainLmjParamDTO.setQuanwangtongYizhifuTransfer("http://www.lemuji.com/qwtys/api/bestpay/transfer_ZNJF.php");
         supplyChainLmjParamDTO.setQuanwangtongYizhifuPartnerid("e40dfb3c25fa31ce3a226695414cbbc3");
-        boolean b = supplyChainLmjService.getCallbackPayStatus(supplyChainLmjParamDTO);
-        Assert.assertTrue(b);
+        SupplyChainLmjResultDTO b = supplyChainLmjService.getCallbackPayStatus(supplyChainLmjParamDTO);
+        Assert.assertNotNull(b);
     }
 
     /**
@@ -90,8 +91,8 @@ public class SupplyChainLmjServiceTest extends JunitBase {
         supplyChainLmjParamDTO.setExternalId("1711271313838686");
         supplyChainLmjParamDTO.setPayCode("000000");
         supplyChainLmjParamDTO.setPayType("payTB");
-        boolean b = supplyChainLmjService.getCallbackPayStatus(supplyChainLmjParamDTO);
-        Assert.assertTrue(b);
+        SupplyChainLmjResultDTO b = supplyChainLmjService.getCallbackPayStatus(supplyChainLmjParamDTO);
+        Assert.assertNotNull(b);
     }
 
     /**
@@ -106,8 +107,8 @@ public class SupplyChainLmjServiceTest extends JunitBase {
         supplyChainLmjParamDTO.setLmjApiSecretKey("77ed904243c2fde0fb309252e98a960c");
         supplyChainLmjParamDTO.setQuanwangtongYizhifuPaytb("http://www.lemuji.com/qwtys/api/bestpay/payTB_ZNJF.php");
         supplyChainLmjParamDTO.setQuanwangtongYizhifuPartnerid("e40dfb3c25fa31ce3a226695414cbbc3");
-        boolean b = supplyChainLmjService.getCallbackPayStatus(supplyChainLmjParamDTO);
-        Assert.assertTrue(b);
+        SupplyChainLmjResultDTO b = supplyChainLmjService.getCallbackPayStatus(supplyChainLmjParamDTO);
+        Assert.assertNotNull(b);
     }
 
 }
