d4e7cb12fef52cd2a739264ce95fa5ce08035326
mwj <yomingyo@gmail.com>
2017-12-25 09:52:12 +0800

    定时任务

--- znjf-external-service/src/main/java/com/znjf33/external/api/provider/service/impl/CallbackExceptionServiceImpl.java
+++ znjf-external-service/src/main/java/com/znjf33/external/api/provider/service/impl/CallbackExceptionServiceImpl.java
@@ -25,7 +25,7 @@ import java.util.concurrent.ConcurrentMap;
 public class CallbackExceptionServiceImpl implements CallbackExceptionService {
 
     @Autowired
-    ZnjfExceptionRecordMapper znjfExceptionRecordMapper;
+    private ZnjfExceptionRecordMapper znjfExceptionRecordMapper;
 
     @Autowired
     private ThirdPartyService thirdPartyService;
